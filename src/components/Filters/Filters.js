751fe27031fed0efa2b025a84cb31a14e1d73daf
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-25 21:40:00 +0200

    Включил в родителя все фильтры

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -18,21 +18,31 @@ import loudSpeaker from "../../static/images/loudSpeaker.svg";
 import AgeFilter from "./components/AgeFilter";
 import PhotoFilter from "./components/PhotoFilter";
 import SexFilter from "./components/SexFilter";
+import FeeFilter from "./components/FeeFilter";
+import ExperienceFilter from "./components/ExperienceFilter";
+import LanguageFilter from "./components/LanguageFilter.jsx";
+import EmploymentType from "./components/EmploymentType";
+import EducationFilter from "./components/EducationFilter";
 
-function Filters({ posts, getSummary, deletePost }) {
-  useEffect(() => {
-    getSummary();
-  }, []);
+function Filters() {
+  // useEffect(() => {
+  //   getSummary();
+  // }, []);
 
   return (
     <Grid item xs={4}>
       <Container>
-        <Typography variant="h4" component="h5">
+        <Typography variant="h4" component="h5" style={{marginBottom:34}}>
           Фильтры
         </Typography>
         <PhotoFilter />
         <AgeFilter />
         <SexFilter />
+        <FeeFilter />
+        <ExperienceFilter />
+        <LanguageFilter />
+        <EmploymentType />
+        <EducationFilter />
       </Container>
     </Grid>
   );
