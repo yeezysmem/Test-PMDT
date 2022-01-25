39ca7e68999254c743e0b304acb0d75f02e2ef79
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-25 21:34:07 +0200

    Компонент фильтра зарплаты

---
+++ src/components/Filters/components/FeeFilter.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+// import './App.css';
+import { connect } from "react-redux";
+import "../../ResumeCard/ResumeCard.scss";
+import {
+  getSummary as getSummaryAction,
+  deletePost as deletePostAction,
+} from "../../../redux/modules/posts";
+import Post from "../../Post";
+import { Container, Grid, Slider, Box, Divider } from "@material-ui/core";
+import {
+  Typography,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+} from "@material-ui/core";
+import loudSpeaker from "../../../static/images/loudSpeaker.svg";
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+function FeeFilter() {
+  const [value, setValue] = React.useState([1100,8980]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Grid item xs={12} style={{marginBottom:55}}>
+      <Container>
+        <Typography variant="h5" component="h5" style={{marginBottom:12}}>
+          Желаемая зарплата
+        </Typography>
+        <Divider light style={{marginBottom:30}}/>
+        {/* <Grid container justifyContent="space-around" alignItems="center"> */}
+          <Slider
+            getAriaLabel={() => "Age range"}
+            value={value}
+            onChange={handleChange}
+            getAriaValueText={valuetext}
+            min={1000}
+            max={10000}
+            step="1"
+          />
+          <Grid container direction="row" alignItems="center" justifyContent="space-between">
+              <Grid item>от</Grid>
+              <Grid item style={{padding: "10px 30px", border:"1px solid #EAEAEA"}}>{value[0]}</Grid>
+              <Grid item>до</Grid>
+              <Grid item style={{padding: "10px 30px", border:"1px solid #EAEAEA"}}>{value[1]}</Grid>
+              <Grid item>грн</Grid>
+          </Grid>
+        {/* </Grid> */}
+      </Container>
+    </Grid>
+  );
+}
+
+export default connect(({ posts }) => ({ posts: posts.posts }), {
+  getSummary: getSummaryAction,
+})(FeeFilter);
