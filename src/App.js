f58ce187874ed25e919db09591b9d205ccf84ecf
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-26 16:09:56 +0200

    Добавил хедер и футер на главную страницу

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <CssBaseline />
       <Container>
         <ResumeCard />
+        
       </Container>
+      <Footer />
     </ThemeProvider>
   );
 }
