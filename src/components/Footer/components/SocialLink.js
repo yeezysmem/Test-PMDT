c43f1fe418f7085f1625f0fd6eb124efa1bdebda
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-26 16:04:30 +0200

    Добавил подкомпоненты для футера

---
+++ src/components/Footer/components/SocialLink.js
@@ -0,0 +1,29 @@
+import { Box, Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Link } from "@material-ui/core";
+import { connect } from "react-redux";
+import {
+  getSummary as getSummaryAction,
+  deletePost as deletePostAction,
+} from "../../../redux/modules/posts";
+
+const useStyles = makeStyles((theme) => ({
+  footerLink: {
+    color: "#fff",
+  },
+}));
+
+function SocialLink({ xs, sm, href="#", icon }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs={xs} sm={sm}>
+      <Link href={href} className={classes.footerLink}>
+        <img src={icon}></img>
+      </Link>
+    </Grid>
+  );
+}
+
+export default connect(({ posts }) => ({ posts: posts.posts }), {
+  getSummary: getSummaryAction,
+})(SocialLink);
