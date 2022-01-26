5e6c7b3bfc7aab533512cf2cf9303906277015c0
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-26 16:06:40 +0200

    Добавил переменную для фильтра в функцию getSummary

--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -1,13 +1,13 @@
 const moduleName = 'posts';
 
-const GET_POSTS = `${moduleName}/GET_POSTS`;
 const GET_SUMMARY = `${moduleName}/GET_SUMMARY`;
-const DELETE_POST = `${moduleName}/DELETE_POST`;
-const CREATE_POST = `${moduleName}/CREATE_POST`;
 
 
 const defaultState = {
   posts: [],
+  currentPage:1,
+  perPage:10,
+  totalCount:0,
 };
 
 /* 
@@ -15,32 +15,16 @@ const defaultState = {
 */
 export default (state = defaultState, { type, payload }) => {
   switch (type) {
-    case GET_POSTS:
-      return { ...state, posts: payload };
     case GET_SUMMARY:
       return { ...state, posts: payload };
-    case DELETE_POST:
-      return { ...state, posts: state.posts.filter(item => item.id !== payload.id) };
-    case CREATE_POST:
-      return { ...state, posts: [...state.posts, payload] };
     default:
       return state;
   }
 };
 
-export const getPosts = () => async (dispatch) => {
+export const getSummary = ( withPhotos ) => async (dispatch) => {
   try {
-    await fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: GET_POSTS, payload: data }))
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-export const getSummary = () => async (dispatch) => {
-  try {
-    await fetch('http://135.181.30.244:27007/api/summaries/?photo=true')
+    await fetch(`http://135.181.30.244:27007/api/summaries/?photo=${withPhotos ? 'true' : 'false'}`)
       .then((response) => response.json())
       .then((data) => dispatch({ type: GET_SUMMARY, payload: data.results }))
   } catch (error) {
@@ -49,37 +33,3 @@ export const getSummary = () => async (dispatch) => {
 }
 
 
-
-
-
-export const deletePost = (id) => async (dispatch) => {
-  try {
-    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-      method: 'DELETE',
-    });
-
-    dispatch({ type: DELETE_POST, payload: { id } })
-  } catch (error) {
-
-  }
-}
-
-export const createPost = ({ title, body }) => async (dispatch) => {
-  try {
-    await fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        body,
-        userId: 1,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: CREATE_POST, payload: data }));
-  } catch (error) {
-    console.log(error)
-  }
-}
