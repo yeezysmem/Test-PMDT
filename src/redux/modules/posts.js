e92bd4e7ba0a2233969ea90422fa406a6263b6e9
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-28 00:07:48 +0200

    Рефакторинг, добавил параметры с апи, и пару пропсов для пагинации

--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -1,3 +1,4 @@
+
 const moduleName = 'posts';
 
 const GET_SUMMARY = `${moduleName}/GET_SUMMARY`;
@@ -5,31 +6,35 @@ const GET_SUMMARY = `${moduleName}/GET_SUMMARY`;
 
 const defaultState = {
   posts: [],
-  currentPage:1,
-  perPage:10,
-  totalCount:0,
+  page_count:12,
+  current_page:1,
+  per_page:10,
+  total_count:0,
+  next:"",
+  previous: "",
 };
 
 /* 
   { type: GET_POSTS, payload: {...} }
 */
-export default (state = defaultState, { type, payload }) => {
+export default function postReducer(state = defaultState, { type, payload }) {
   switch (type) {
     case GET_SUMMARY:
-      return { ...state, posts: payload };
+      return { ...state, posts: payload.results,  total_count: payload.total_count,next: payload.next, previous: payload.previous };
     default:
       return state;
   }
 };
 
-export const getSummary = ( withPhotos ) => async (dispatch) => {
+export const getSummary = ( {withPhotos, current_page, url } ) => async (dispatch) => {
   try {
-    await fetch(`http://135.181.30.244:27007/api/summaries/?photo=${withPhotos ? 'true' : 'false'}`)
+    await fetch(url || `http://135.181.30.244:27007/api/summaries/?${current_page ? `page=${current_page}` : '' }&photo=${withPhotos ? 'true' : 'false'}`)
       .then((response) => response.json())
-      .then((data) => dispatch({ type: GET_SUMMARY, payload: data.results }))
+      .then((data) => dispatch({ type: GET_SUMMARY, payload: data }))
   } catch (error) {
-    console.log(error)
+    console.log(error)  
   }
 }
 
 
+//total/10 - всего страниц 
