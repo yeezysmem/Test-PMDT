91ae5dea2cf9b0d91526c6596522ea6be4d13f30
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-28 00:04:27 +0200

    Рефакторинг кода

--- src/components/Footer/components/Privacy.js
+++ src/components/Footer/components/Privacy.js
@@ -1,25 +1,41 @@
-import { Box, Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Link } from "@material-ui/core";
+import { Grid, Link } from "@material-ui/core";
 import { connect } from "react-redux";
-import {
-  getSummary as getSummaryAction,
-  deletePost as deletePostAction,
-} from "../../../redux/modules/posts";
+import { getSummary as getSummaryAction } from "../../../redux/modules/posts";
 
 const useStyles = makeStyles((theme) => ({
   footerLink: {
     color: "#fff",
   },
+  navLinkContainer: {
+    display: "flex",
+    padding: "13px 33px",
+    alignItems: "center",
+  },
+  horisontalDivider: {
+    background: "#fff",
+    height: 12,
+    width: 1,
+  },
 }));
 
-function Footer({ xs, sm, href="#", icon }) {
+function Footer({ xs, sm, href = "#", icon }) {
   const classes = useStyles();
   return (
-    <Grid item xs={xs} sm={sm}>
-      <Link href={href} className={classes.footerLink}>
-        <img src={icon}></img>
-      </Link>
+    <Grid container justify="space-between">
+      <Grid className={classes.navLinkContainer} item>
+        <Link href="#" variant="subtitle2" className={classes.footerLink}>
+          Соискатель
+        </Link>
+        <span className={classes.horisontalDivider} />
+        <Link href="#" variant="subtitle2" className={classes.footerLink}>
+          Работодатель
+        </Link>
+        <span className={classes.horisontalDivider} />
+        <Link href="#" variant="subtitle2" className={classes.footerLink}>
+          HR
+        </Link>
+      </Grid>
     </Grid>
   );
 }
