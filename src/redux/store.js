d9600c96c4ac699f733524ffd295a45404b4db57
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2022-01-28 00:05:10 +0200

    Добавил настройку для Redux DevTools Extension в браузере

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,17 +4,20 @@ import reduxLogger from 'redux-logger';
 
 import rootReducers from './modules';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const configureStore = (reducers = {}, preloadedState = {}, middlewares = []) => createStore(
   combineReducers({
     ...rootReducers,
-    ...reducers
+    ...reducers,
   }),
   preloadedState,
-  compose(
+  composeEnhancers(
     applyMiddleware(
       ...middlewares,
       thunk,
-      reduxLogger
+      reduxLogger,
+
     ),
   )
 );
